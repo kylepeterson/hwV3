d1e392b9fd6a57c9b8e88f8872c2a8d86652caab
Kyle Peterson <kylep9@uw.edu>
2013-11-06 00:30:12 -0800

    attempt at drinks and desserts

--- js/populateMenu.js
+++ js/populateMenu.js
@@ -1,12 +1,14 @@
 // On Document Ready
 $(function() {
 	populatePizzas();
+	populateDrinks();
+	populateDesserts();
 });
 
 function populatePizzas() {
 	var instance;
 	$.each(com.dawgpizza.menu.pizzas, function() {
-    	instance = $('.template').clone();
+    	instance = $('.pizzas .template').clone();
     	instance.find('.name').html(this.name);
        	instance.find('.description').html(this.description);
     	instance.find('.prices').html('$' + this.prices[0] + '/$' + this.prices[1] + '/$' + this.prices[2]);
@@ -17,5 +19,27 @@ function populatePizzas() {
 	    else {
 	    	$('.meat-list').append(instance);
 	    }
+	});
+}
+
+function populateDrinks() {
+	var instance;
+	$.each(com.dawgpizza.menu.drinks, function() {
+    	instance = $('.drinks-and-dessert .template').clone();
+    	instance.find('.name').html(this.name);
+    	instance.find('.prices').html('$' + this.price);
+    	instance.removeClass('template');
+	    $('.drinks').append(instance);
+	});
+}
+
+function populateDesserts() {
+	var instance;
+	$.each(com.dawgpizza.menu.desserts, function() {
+    	instance = $('.drinks-and-dessert .template').clone();
+    	instance.find('.name').html(this.name);
+    	instance.find('.prices').html('$' + this.price);
+    	instance.removeClass('template');
+	    $('.desserts').append(instance);
 	});
 }
