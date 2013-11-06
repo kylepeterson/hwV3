bb2f9d97d5eb693197e8963cc72d7082180983c8
Kyle Peterson <kylep9@uw.edu>
2013-11-05 23:49:11 -0800

    no template append

--- js/populateMenu.js
+++ js/populateMenu.js
@@ -15,6 +15,15 @@ function populatePizzas() {
     	instance.removeClass('template');*/
     	var name = $(document.createElement('dt'))
     	name.html(pizza.name);
+    	var description = $(document.createElement('dd'));
+    	description.html(pizza.description);
+    	var prices = $(document.createElement('dd'));
+    	prices.html('$' + pizza.prices[0] + '/$' + pizza.prices[1] + '/$' + pizza.prices[2]);
     	$('.meat-list').append(name);
+    	$('.meat-list').append(description);
+    	$('.meat-list').append(prices);
+
+
+
 	} //for each pizza
 }
