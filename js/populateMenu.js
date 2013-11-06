9956b8c7f0cdbade13250566e77e3d4b72d0f9b7
Kyle Peterson <kylep9@uw.edu>
2013-11-05 22:29:24 -0800

    no message

---
+++ js/populateMenu.js
@@ -0,0 +1,15 @@
+$(function() {
+	var i;
+	var pizza;
+	for (i = 0; i < com.dawgpizza.menu.pizzas.length; i++) {
+    	pizza = com.dawgpizza.menu.pizzas[i];
+
+	    //pizza.name = name of pizza
+	    //pizza.description = description of pizza
+	    //pizza.prices = array of three numbers, which are prices for small, medium, and large
+	    //pizza.prices[0] = price for small
+	    //pizza.prices[1] = price for medium
+	    //pizza.prices[2] = price for large
+
+	} //for each pizza
+});
