71720a4db45b6f0de8d1c2f25cd3291d34b7b716
Kyle Peterson <kylep9@uw.edu>
2013-11-06 22:13:41 -0800

    no message

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,15 @@
 $(function() {
 	//Stops the carousel from progressing if on hte last slide
-	$('#pizza-carousel').on('slid', '', function() {
-	  	if($('.carousel-inner .item:last').hasClass('active')) {
-	    	$(this).carousel('pause');
-	  	}
+	$('#myCarousel').on('slid', '', function() {
+  		var $this = $(this);
+
+  		if($('.carousel-inner .item:first').hasClass('active')) {
+    		$this.children('.left').hide();
+  		} else if($('.carousel-inner .item:last').hasClass('active')) {
+    		$this.children('.right').hide();
+  		} else {
+    		$this.children('.carousel-control').show();
+  		}
+
 	});
 });
