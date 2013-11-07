36c6ad40dc9c5c109de6b4284935d54a35f73186
Kyle Peterson <kylep9@uw.edu>
2013-11-06 22:00:27 -0800

    javascript to tweak carousel

---
+++ js/carousel.js
@@ -0,0 +1,8 @@
+$(function() {
+	//Stops the carousel from progressing if on hte last slide
+	$('#myCarousel').on('slid', '', function() {
+	  	if($('.carousel-inner .item:last').hasClass('active')) {
+	    	$(this).carousel('pause');
+	  	}
+	});
+});
