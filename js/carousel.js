9d1bd537f96720129aa4911f74ab02a2aa021dd4
Kyle Peterson <kylep9@uw.edu>
2013-11-06 22:09:49 -0800

    no message

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,6 @@
 $(function() {
 	//Stops the carousel from progressing if on hte last slide
-	$('#myCarousel').on('slid', '', function() {
+	$('#pizza-carousel').on('slid', '', function() {
 	  	if($('.carousel-inner .item:last').hasClass('active')) {
 	    	$(this).carousel('pause');
 	  	}
